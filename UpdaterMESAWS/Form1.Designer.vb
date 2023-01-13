a517380c2afc907f911fe1d66ceaf21bce153061
Bepo <81547893+santo-polibatam@users.noreply.github.com>
2023-01-13 17:03:02 +0700

    V1

--- UpdaterMESAWS/Form1.Designer.vb
+++ UpdaterMESAWS/Form1.Designer.vb
@@ -22,10 +22,88 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Progress_download = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Progress_download
+        '
+        Me.Progress_download.Location = New System.Drawing.Point(23, 47)
+        Me.Progress_download.Name = "Progress_download"
+        Me.Progress_download.Size = New System.Drawing.Size(250, 23)
+        Me.Progress_download.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(198, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Download"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(12, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "-"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(23, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "-"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(261, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "-"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(298, 125)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Progress_download)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Updating..."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Progress_download As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
 End Class
--- UpdaterMESAWS/Form1.vb
+++ UpdaterMESAWS/Form1.vb
@@ -1,3 +1,116 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Net
+Imports System.Reflection.Emit
+Imports System.Xml.XPath
+Imports System.Xml
+Imports System.Threading
 
+Public Class Form1
+
+    Dim delay_timer As Integer = 0
+
+    Public Sub UnZip()
+
+        'Dim startPath As String = "c:\example\start"
+        Dim zipPath As String = Application.StartupPath() & "/main.zip"
+        Dim extractPath As String = Application.StartupPath()
+
+        ZipFile.ExtractToDirectory(zipPath, extractPath)
+
+    End Sub
+    Public Sub deletingFile()
+        Try
+            If System.IO.File.Exists(Application.StartupPath() & "/main.zip") = True Then
+                Label2.Text = "Deleting !"
+                System.IO.File.Delete(Application.StartupPath() & "/main.zip")
+                System.IO.Directory.Delete(Application.StartupPath() & "/MESAWSSG_Build-Main", True)
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Public Sub download_update()
+        Dim wc As New WebClient
+        deletingFile()
+        AddHandler wc.DownloadProgressChanged, AddressOf downloadProgressChanged
+        wc.DownloadFileAsync(New System.Uri("https://codeload.github.com/SantoSanindo/MESAWSSG_Build/zip/refs/heads/main"), Application.StartupPath() & "/main.zip")
+    End Sub
+
+    Public Sub downloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        Label2.Text = "Downloading !"
+        Progress_download.Value = e.ProgressPercentage
+        Label1.Text = e.BytesReceived & " / " & e.TotalBytesToReceive
+        If e.BytesReceived = e.TotalBytesToReceive Then
+            System.Threading.Thread.Sleep(5000)
+            Label2.Text = "UnZiping !"
+            UnZip()
+            Label2.Text = "Copying !"
+            CopyDirectory(Application.StartupPath() & "/MESAWSSG_Build-Main", Application.StartupPath())
+            Label2.Text = "Deleting !"
+            deletingFile()
+            Label2.Text = "Finish !"
+
+        End If
+
+    End Sub
+    Public Sub CopyDirectory(ByVal sourcePath As String, ByVal destinationPath As String)
+
+
+        If System.IO.Directory.Exists(sourcePath) Then
+            Dim sourceDirectoryInfo As New System.IO.DirectoryInfo(sourcePath)
+
+            ' If the destination folder don't exist then create it
+            If Not System.IO.Directory.Exists(destinationPath) Then
+                'System.IO.Directory.CreateDirectory(destinationPath)
+                MessageBox.Show("Directory doesn't exist !")
+                Exit Sub
+            End If
+
+            Dim fileSystemInfo As System.IO.FileSystemInfo
+            For Each fileSystemInfo In sourceDirectoryInfo.GetFileSystemInfos
+
+                'MessageBox.Show(fileSystemInfo.FullName)
+
+                Dim destinationFileName As String =
+                    System.IO.Path.Combine(destinationPath, fileSystemInfo.Name)
+
+                ' Now check whether its a file or a folder and take action accordingly
+                If TypeOf fileSystemInfo Is System.IO.FileInfo Then
+                    System.IO.File.Copy(fileSystemInfo.FullName, destinationFileName, True)
+                Else
+                    ' Recursively call the mothod to copy all the neste folders
+                    'CopyDirectory(fileSystemInfo.FullName, destinationFileName)
+                End If
+                Label2.Text = fileSystemInfo.FullName
+                Application.DoEvents()
+            Next
+
+        Else
+            Label2.Text = "Updating Failed !"
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        download_update()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        delay_timer = delay_timer + 1
+        Label3.Text = delay_timer
+        If delay_timer = 5 Then Button1_Click(sender, e)
+        If delay_timer >= 15 And Label2.Text = "Finish !" Then
+            Dim proc As New System.Diagnostics.Process()
+            proc = Process.Start("MES APP.exe", "")
+            Me.Close()
+        End If
+    End Sub
 End Class
